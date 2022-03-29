75a0bcb62894c712fd23aac4d3f867137ac9d422
Frédéric Briol <fbriol@gmail.com>
2022-03-29 11:17:54 +0200

    :memo: Publishing documentation.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,6 +38,7 @@ version = '.'.join(release.split('.')[:2])
 # extensions coming with Sphinx (named "sphinx.ext.*") or your custom
 # ones.
 extensions = [
+    'sphinx_inline_tabs',
     "sphinx_gallery.gen_gallery",
     "sphinx.ext.autodoc",
     "sphinx.ext.autosummary",
@@ -75,7 +76,8 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "nature"
+html_theme = 'furo'
+html_title = 'ZCollection'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
